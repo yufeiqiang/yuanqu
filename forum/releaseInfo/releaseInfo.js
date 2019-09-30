0cd58f1355047a8321dac10c1202c8cbfa7761f7
yufeiqiang <l446731380@qq.com>
2019-09-30 15:59:59 +0800

    发布功能

--- forum/releaseInfo/releaseInfo.js
+++ forum/releaseInfo/releaseInfo.js
@@ -1,66 +1,128 @@
-// forum/releaseInfo/releaseInfo.js
+const app = getApp()
+const request = require("../../utils/request.js");
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    
+    baseUrl: app.globalData.baseUrl,
+    userId: app.globalData.user.memberId,
+    param:{
+      content:'',
+      type:'1',
+      createBy: app.globalData.user.memberId,
+      pics: [],
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 发布信息
    */
-  onReady: function () {
-
+  releaseInfo(){
+    if (this.data.param.content == ''){
+      wx.showToast({
+        title: '内容不能空信息'
+      })
+    }else{
+      let param = Object.assign({}, this.data.param)
+      param.pics = param.pics.join()
+      request.postRequest('bbs/info/bbsInfo/crud/save', param).then(res => {
+        if (res.code == 200) {
+          wx.navigateTo({
+            url: '../index/index',
+          })
+        }
+      })
+    }
   },
-
   /**
-   * 生命周期函数--监听页面显示
+   * 文本域内容改变
    */
-  onShow: function () {
-
+  formInputChange(e) {
+    const comment = e.detail.value
+    this.setData({
+      'param.content': comment
+    })
   },
-
   /**
-   * 生命周期函数--监听页面隐藏
+   * 点击上传图片
    */
-  onHide: function () {
-
+  chooseImg() {
+    let that = this;
+    let len = this.data.param.pics;
+    if (len >= 9) {
+      this.setData({
+        lenMore: 1
+      })
+      return;
+    }
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      success: (res) => {
+        let tempFilePaths = res.tempFilePaths
+        // console.log(res);
+        let pics = that.data.param.pics;
+        for (let i = 0; i < tempFilePaths.length; i++) {
+          if (pics.length < 9) {
+            wx.showLoading({
+              title: '上传中',
+            })
+            wx.uploadFile({
+              url: that.data.baseUrl +'/park_manage/api/sys/file/upload',
+              filePath:tempFilePaths[i],
+              name:'file',
+              header:'',
+              formData:{
+                accessToken: app.globalData.user.accessToken,
+                userId: app.globalData.user.memberId,
+                dfsType:'0'
+              },
+              success:function(e){
+                let data = JSON.parse(e.data)
+                if(e.statusCode == 200){
+                  if(data.code==200){
+                    pics.push(data.data.file_rsurl)
+                    that.setData({
+                      'param.pics':pics
+                    });
+                    wx.hideLoading()
+                  }
+                }
+                
+              }
+            })
+          } else {
+            that.setData({
+              'param.pics':pics
+            })
+            wx.showModal({
+              title: '提示',
+              content: '最多只能有九张图片'
+            })
+            return;
+          }
+        }
+        that.setData({
+          'param.pics':pics
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  previewImg(e) {
+    let index = e.currentTarget.dataset.index;
+    let pics = this.data.param.pics;
+    wx.previewImage({
+      current: pics[index],
+      urls: pics,
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  deleteImg(e) {
+    let _index = e.currentTarget.dataset.index;
+    console.log(e)
+    let pics = this.data.param.pics;
+    pics.splice(_index, 1);
+    this.setData({
+      'param.pics':pics
+    })
   }
 })
